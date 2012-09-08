370705b382a1dd1b97b901b2b2d3f793dbd1036a
hirokuma <uenokuma@gmail.com>
2012-09-08 09:01:07 +0900

    エラー番号表示

--- src/rcs620s_sample.c
+++ src/rcs620s_sample.c
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2012-2012, hiro99ma(uenokuma@gmail.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *         this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *         this list of conditions and the following disclaimer
+ *         in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
 #include <kernel.h>
 #include "kernel_cfg.h"
 #include "fm3_mb9bxxx.h"
@@ -44,10 +69,15 @@ void main_task(intptr_t exinf)
 	st7032i_write_string(STR_TITLE);
 
 	while(1) {
+		//カード探索して・・・
 		type = HkNfcRw_Detect(true, true, true);
+		//すぐにRFは止めて・・・
 		HkNfcRw_RfOff();
+		//カード種別ごとに処理
 		st7032i_move_pos(1, 1);
 		switch(type) {
+
+		// NFC-Aの場合、TPEだったらSNEP(text)
 		case HKNFCTYPE_A:
 			cq_frk_fm3_led_ctrl(CQFRKFM3_LED_BLINK, 250, 3000);
 			st7032i_write_string(STR_NFCA);
@@ -58,10 +88,14 @@ void main_task(intptr_t exinf)
 				}
 			}
 			break;
+
+		// NFC-Bはカード
 		case HKNFCTYPE_B:
 			cq_frk_fm3_led_ctrl(CQFRKFM3_LED_BLINK, 250, 3000);
 			st7032i_write_string(STR_NFCB);
 			break;
+
+		// NFC-Aの場合、TPEだったらSNEP(text)
 		case HKNFCTYPE_F:
 			cq_frk_fm3_led_ctrl(CQFRKFM3_LED_BLINK, 200, 3000);
 			st7032i_write_string(STR_NFCF);
@@ -142,7 +176,18 @@ static void _snep(const char* pStr)
 static void _snep_fail(const char *pStr)
 {
 	cq_frk_fm3_led_ctrl(CQFRKFM3_LED_BLINK, 250, 5000);
+
+	const char HEX[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	uint8_t err = HkNfcRw_GetLastError();
+	char hex[4];
+	hex[0] = HEX[err >> 4];
+	hex[1] = HEX[err & 0x0f];
+	hex[2] = ' ';
+	hex[3] = '\0';
 	st7032i_move_pos(0, 0);
+	st7032i_write_string(hex);
+	
+	st7032i_move_pos(3, 0);
 	st7032i_write_string(pStr);
 	
 	dly_tsk(10000);
